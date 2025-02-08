4d131003d804a972a4afa1dd6e94003aead0e3ca
Henry LeCompte <henry@lunar-glass.com>
2025-02-07 23:44:56 -0500

    Score for longer

--- src/main/java/frc/robot/commands/QuarrelCommands.java
+++ src/main/java/frc/robot/commands/QuarrelCommands.java
@@ -69,7 +69,7 @@ public class QuarrelCommands {
                     .or(subsystem.bottomBeamBreak.getTrigger())
                     .negate()),
             new FlywheelVoltageCommand(
-                subsystem.claw, () -> subsystem.pivot.getPosition() > 0.5 ? -3.0 : 3.0)),
+                subsystem.claw, () -> subsystem.pivot.getPosition() > 0.5 ? -7.0 : 7.0)),
         new WaitCommand(1),
         new FlywheelVoltageCommand(subsystem.claw, () -> 0.0).withTimeout(0.02));
   }
