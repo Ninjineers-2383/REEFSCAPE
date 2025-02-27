57d27018882d4fbd706e6db947450f288c35c4c2
Henry LeCompte <henry@lunar-glass.com>
2025-02-27 15:16:20 -0500

    Update at 'Thu Feb 27 15:16:20 EST 2025'

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -17,7 +17,7 @@ public class VisionConstants {
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d robotToCamera0 =
-      new Transform3d(0, 0.2, 0.0, new Rotation3d(0.0, 0, Math.PI / 2));
+      new Transform3d(0.2, 0.2, 0.0, new Rotation3d(0.0, 0, Math.PI / 2));
   public static Transform3d robotToCamera1 =
       new Transform3d(0.1, -0.22, 0.2, new Rotation3d(0.0, 0.0, 0.0));
 
