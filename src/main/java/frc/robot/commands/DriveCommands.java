f028aa193584bf2da4754535c475b27bb7fd1e6a
Henry LeCompte <henry@lunar-glass.com>
2025-02-27 22:50:40 -0500

    Revert "Update at 'Thu Feb 27 18:31:09 EST 2025'"
    This reverts commit 11ac0a4c5e2edd01df655f50ea7564f995e2ed56.

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -407,11 +407,9 @@ public class DriveCommands {
 
           if (dot > 0.5 && errorTranslation.getNorm() < 1) {
             speeds =
-                // (trajectoryVelocity.times(dot * dot *
-                // dot).times(Vdrive.getTranslation().getNorm()))
-                //     .plus(driverVelocity.times((1 - dot) * (1 - dot) * (1 - dot)))
-                //     .plus(error);
-                driverVelocity;
+                (trajectoryVelocity.times(dot * dot * dot).times(Vdrive.getTranslation().getNorm()))
+                    .plus(driverVelocity.times((1 - dot) * (1 - dot) * (1 - dot)))
+                    .plus(error);
 
             Logger.recordOutput("DriveAlongTrajectory/Enabled", true);
 
