b43065a2bbfaa77f72c7431057085098e77155fe
Henry LeCompte <henry@lunar-glass.com>
2025-03-06 17:33:21 -0500

    Update at 'Thu Mar 06 17:33:20 EST 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -114,7 +114,7 @@ public class RobotContainer {
         // the Phoenix Odometry Thread, if using a combination of the two, set up both
         drive =
             new Drive(
-                new GyroIOPigeon2(0, "Drive"),
+                new GyroIO() {},
                 new Module(
                     new DriveMotorIOTalonFX(
                         "FrontLeftDrive", DriveMotorConstants.FRONT_LEFT_CONFIG),
