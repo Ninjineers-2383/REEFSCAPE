d888847cb7422b8e0a926124d20fce38022ec729
Henry LeCompte <henry@lunar-glass.com>
2025-04-04 16:31:20 -0400

    Update at 'Fri Apr 04 16:31:19 EDT 2025'

--- src/main/java/frc/robot/subsystems/position_joint/PositionJointConstants.java
+++ src/main/java/frc/robot/subsystems/position_joint/PositionJointConstants.java
@@ -74,7 +74,7 @@ public class PositionJointConstants {
           GravityType.CONSTANT,
           EncoderType.EXTERNAL_CANCODER_PRO,
           17,
-          Rotation2d.fromRotations(-0.264160 - 0.25),
+          Rotation2d.fromRotations(-0.128418 - 0.25),
           "Drive");
 
   public static final PositionJointGains CLIMBER_GAINS =
